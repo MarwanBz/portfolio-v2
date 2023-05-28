cc39d1898031a495dd2ed99058e258b3b3553f14
MarwanBz <69892218+MarwanBz@users.noreply.github.com>
2023-05-28 18:30:11 +0300

    chore: update and del dublicated dist folder

--- dist/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import App from './App'
-import Maintenance from './components/UnderBuilding/index'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-const isUnderMaintenance = true
-ReactDOM.createRoot(document.getElementById("root")).render(
-    isUnderMaintenance ? <Maintenance /> : <App />
-);
