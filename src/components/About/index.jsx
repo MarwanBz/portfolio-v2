1820e063fe295200a204e1af9772518f6a70b9a0
MarwanBz <69892218+MarwanBz@users.noreply.github.com>
2023-06-16 20:48:35 +0300

    chore: optimazie circle svg code for animations

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,8 +1,16 @@
 import "./about.scss";
 
 import Title from "../sectionTitle";
+import { gsap } from "gsap";
+import { useEffect } from "react";
+import { useRef } from "react";
 
 function About(props) {
+  console.log(props);
+  useEffect(() => {
+    console.log("useEffect ran");
+  });
+  console.log("outside useEffect");
   return (
     <section id="about">
       <div className="container">
@@ -21,20 +29,34 @@ function About(props) {
             <img src="/src/assets/images/marwan.png" alt="" />
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="360"
-              height="360"
-              viewBox="0 0 360 360"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              width="380.031"
+              height="416.003"
+              viewBox="0 0 380.031 416.003"
             >
-              <g
+              <defs>
+                <linearGradient
+                  id="linear-gradient"
+                  x1="0.842"
+                  y1="1.129"
+                  x2="0.348"
+                  y2="0.348"
+                  gradientUnits="objectBoundingBox"
+                >
+                  <stop offset="0" stop-color="#55c8c4" stop-opacity="0" />
+                  <stop offset="1" stop-color="#72b4bd" stop-opacity="0" />
+                </linearGradient>
+              </defs>
+              <path
                 id="img-circle"
-                fill="none"
+                d="M67.081,18.394c32.713,6.758,55.975,35.692,79.152,66.273,28.753,37.939,68.8,71.737,66.117,125.409-3.023,60.507-37.192,113.607-79.363,139.726-41.631,25.784-92.123,22.444-130.352-9.839-33.811-28.553-92.32-59.895-102.163-111.023C-109.946,174.823-81.019,87.694-54.495,43.2-27.871-1.461,24.642,9.627,67.081,18.394"
+                transform="translate(108.955 28.849) rotate(-8)"
                 stroke="#b97051"
-                stroke-width="17"
-                stroke-dasharray="82 99"
-              >
-                <circle cx="180" cy="180" r="180" stroke="none" />
-                <circle cx="180" cy="180" r="171.5" fill="none" />
-              </g>
+                strokeLinecap="round"
+                strokeWidth="17"
+                strokeDasharray="133 100"
+                fill="url(#linear-gradient)"
+              />
             </svg>
           </div>
         </div>
