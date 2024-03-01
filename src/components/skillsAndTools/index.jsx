391b8abd78d988f1735c517b4eb612de93b9d566
Marwan <kaka.mer1998@gmail.com>
2024-03-02 00:42:25 +0300

    chore: add yarn

--- src/components/skillsAndTools/index.jsx
+++ src/components/skillsAndTools/index.jsx
@@ -6,7 +6,7 @@ function SkillsAndTools() {
   return (
     <section id="tools">
       <div className="container">
-        <img src="/src/assets/images/toolsAndskills.svg" alt="" />
+        {/* <img src="/src/assets/images/toolsAndskills.svg" alt="" /> */}
         <div className="tools-container">
           <Title title="Tools and Skills"/>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus facere commodi dignissimos error laborum suscipit velit quas ea provident. Temporibus aperiam in hic ex, exercitationem debitis maxime veniam reiciendis dignissimos.</p>
