c76d66a112e840db972385d98022158735f8e2ae
Marwan <kaka.mer1998@gmail.com>
2024-09-07 17:56:47 +0300

    feat: git back to work  :D

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -8,9 +8,9 @@ import { useRef } from "react";
 function About(props) {
   // ! svg circle length = 1040.5220947265625
   useEffect(() => {
-    const svg = document.getElementById("img-circle");
-    const l = svg.getTotalLength();
-    console.log(l);
+    // const svg = document.getElementById("img-circle");
+    // const l = svg.getTotalLength();
+    // console.log(l);
     
     
   });
@@ -38,35 +38,34 @@ function About(props) {
           <div className="about-img">
             <img src="/src/assets/images/marwan.png" alt="" />
             <svg
+              width="350"
+              height="400"
+              viewBox="0 0 350 400"
+              fill="none"
               xmlns="http://www.w3.org/2000/svg"
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              width="380.031"
-              height="416.003"
-              viewBox="0 0 380.031 416.003"
             >
+              <rect
+                x="15"
+                y="15"
+                width="320"
+                height="370"
+                rx="46"
+                stroke="url(#paint0_linear_1_2565)"
+                stroke-width="30"
+              />
               <defs>
                 <linearGradient
-                  id="linear-gradient"
-                  x1="0.842"
-                  y1="1.129"
-                  x2="0.348"
-                  y2="0.348"
-                  gradientUnits="objectBoundingBox"
+                  id="paint0_linear_1_2565"
+                  x1="175"
+                  y1="0"
+                  x2="175"
+                  y2="400"
+                  gradientUnits="userSpaceOnUse"
                 >
-                  <stop offset="0" stopColor="#55c8c4" stopOpacity="0" />
-                  <stop offset="1" stopColor="#72b4bd" stopOpacity="0" />
+                  <stop stop-color="#6797DC" />
+                  <stop offset="1" stop-color="#F881E6" />
                 </linearGradient>
               </defs>
-              <path
-                id="img-circle"
-                d="M67.081,18.394c32.713,6.758,55.975,35.692,79.152,66.273,28.753,37.939,68.8,71.737,66.117,125.409-3.023,60.507-37.192,113.607-79.363,139.726-41.631,25.784-92.123,22.444-130.352-9.839-33.811-28.553-92.32-59.895-102.163-111.023C-109.946,174.823-81.019,87.694-54.495,43.2-27.871-1.461,24.642,9.627,67.081,18.394"
-                transform="translate(108.955 28.849) rotate(-8)"
-                stroke="#b97051"
-                strokeLinecap="round"
-                strokeWidth="17"
-                strokeDasharray="133 100"
-                fill="url(#linear-gradient)"
-              />
             </svg>
           </div>
         </div>
